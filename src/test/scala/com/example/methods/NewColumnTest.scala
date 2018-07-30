a54c9f02e7958c44a3cbf73980f9ed9ae28ae3ae
treilly94 <treilly94@live.com>
2018-07-30 15:37:36 +0100

    Added a test for newColumn

---
+++ src/test/scala/com/example/methods/NewColumnTest.scala
@@ -0,0 +1,16 @@
+package com.example.methods
+
+import org.apache.spark.sql.SparkSession
+import org.scalatest.{FunSuite, Matchers}
+
+class NewColumnTest extends FunSuite with Matchers{
+
+  def testAddColumn(): Unit = {
+
+//    val spark: SparkSession = SparkSession.builder().getOrCreate()
+//
+//    println(spark.version)
+//    spark.version should be ("2.2.0")
+    print(1)
+  }
+}
