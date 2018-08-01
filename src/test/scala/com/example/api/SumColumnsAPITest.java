a7d7bbc35c3674682615307aaa7d497de443cb1a
treilly94 <treilly94@live.com>
2018-08-01 15:42:19 +0100

    Added scala api test

---
+++ src/test/scala/com/example/api/SumColumnsAPITest.java
@@ -0,0 +1,31 @@
+package com.example.api;
+
+import com.example.SumColumns;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SumColumnsAPITest {
+
+    private SparkSession spark = SparkSession.builder().master("local").getOrCreate();
+
+    @Test
+    public void sumColumns() {
+        Dataset<Row> input = spark.read().json("./src/test/resources/input/sum_columns.json");
+        System.out.println("Input");
+        input.show();
+
+        Dataset<Row> expected = spark.read().json("./src/test/resources/expected/sum_columns.json");
+        System.out.println("Expected");
+        expected.show();
+
+        Dataset<Row> output = SumColumns.sumColumns(input, "col1", "col2", "sum");
+        System.out.println("Output");
+        output.show();
+
+        assertEquals(expected.collectAsList(), output.collectAsList());
+    }
+}
