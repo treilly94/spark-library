147e2f59f82869267c145df3ca36e7f9eda26323
treilly94 <treilly94@live.com>
2018-08-01 16:01:17 +0100

    Added sql context

--- python/methods/sum_columns.py
+++ python/methods/sum_columns.py
@@ -5,4 +5,4 @@ def sum_columns(df, col1, col2, new_col):
     # Get the Scala API
     api = df._sc._jvm.com.example.api.SumColumnsAPI
 
-    return DataFrame(api.sumColumns(df._jdf, col1, col2, new_col))
+    return DataFrame(api.sumColumns(df._jdf, col1, col2, new_col), df.sql_ctx)
