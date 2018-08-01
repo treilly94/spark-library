95f6b9309a186cd79aaf51c9b7ffacada6f0abd1
treilly94 <treilly94@live.com>
2018-08-01 13:18:52 +0100

    Added python tests

---
+++ python/tests/test_sum_columns.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from pyspark.sql import SparkSession
+
+from methods.sum_columns import sum_columns
+
+
+class TestSum_columns(TestCase):
+
+    def test_sum_columns(self):
+        spark = SparkSession.builder.getOrCreate()
+        #spark.sparkContext.setLogLevel("ERROR")
+
+        # input = spark.read.json("src/test/resources/input/sum_columns.json")
+        # print("Input")
+        # input.show()
+        # expected = spark.read.json("src/test/resources/expected/sum_columns.json")
+        # print("Expected")
+        # expected.show()
+        # output = sum_columns(input, "col1", "col2", "sum")
+        # print("Output")
+        # output.show()
+
+        self.assertEqual()
