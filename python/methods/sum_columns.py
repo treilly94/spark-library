5aebcb8dba0e1c32530d62bf6f977cb9e353bdf7
treilly94 <treilly94@live.com>
2018-08-01 12:44:33 +0100

    Added python wrapper

---
+++ python/methods/sum_columns.py
@@ -0,0 +1,8 @@
+from pyspark.sql import DataFrame
+
+
+def sum_columns(df, col1, col2, new_col):
+    # Get the Scala API
+    api = df._sc._jvm.com.example.api.SumColumnsAPI
+
+    return DataFrame(api.sumColumns(df._jdf, col1, col2, new_col))
