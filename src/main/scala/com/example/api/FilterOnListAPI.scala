d4ab96667f9edc298be9f23ca9d0a4fa74691c31
GMReynolds <genmreynolds@hotmail.co.uk>
2018-08-13 20:14:57 +0100

    Wrote api tests

--- src/main/scala/com/example/api/FilterOnListAPI.scala
+++ src/main/scala/com/example/api/FilterOnListAPI.scala
@@ -8,7 +8,7 @@ import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import scala.collection.JavaConversions._
 
 object FilterOnListAPI {
-  def filterOnList(df: Dataset[Row], targetCol: String, values: util.ArrayList[Int]): DataFrame = {
-    FilterOnList.filterOnList(df, targetCol, values.toList)
+  def filterOnList(df: Dataset[Row], targetCol: String, values: util.ArrayList[Integer]): DataFrame = {
+    FilterOnList.filterOnList(df, targetCol, values.map(_.toInt).toList)
   }
 }
