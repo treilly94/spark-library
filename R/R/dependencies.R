564eb6e84949d9420aa8b456c24502f3f34c9690
Kayleigh Bellis <bellis.kayleigh@gmail.com>
2018-08-14 17:41:18 +0100

    adding dependencies file for the jar

---
+++ R/R/dependencies.R
@@ -0,0 +1,17 @@
+spark_dependecies <- function(spark_version, scala_version, ...){
+  sparklyr::spark_dependency(
+    jars = c(
+      system.file(
+        "../../target/spark-library-1.0-SNAPSHOT.jar",
+        package="spark-library"
+      )
+    ),
+    packages = c()
+  )
+
+}
+
+#' @import sparkylr
+.onload <- function(libname, pkgname) {
+  sparklyr::register_extension(pkgname)
+}
